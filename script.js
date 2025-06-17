b5240fff16f80c1aeabd078c2d6a25db80b38401
Athul <athuldevan90@gmail.com>
2025-06-17 08:57:54 +0530

    first commit

--- script.js
+++ script.js
@@ -16,14 +16,21 @@ function sendMail() {
   const serviceID = "service_rza28en";
   const templateID = "template_t52ar4s";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";bnnnn
-        console.log(res);
-        alert("Your message sent successfully!!")
-
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      bnnnn;
+      console.log(res);
+      alert("Your message sent successfully!!");
     })
-    .catch(err=>console.log(err));
-  }
+    .catch((err) => console.log(err));
+}
+
+// prventing to defualt behaviour of the form
+document.querySelector(".form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent page reload
+  sendMail(); // Your function (optional)
+});
